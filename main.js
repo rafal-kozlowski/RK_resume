478fb1ab472feade323382cb93a3e10e8f5b8022
Rafal Kozlowski <raffal.kozlowski@gmail.com>
2022-08-31 22:00:53 +0200

    add software skills hiding button

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
-//========== showing button ==========//
-let show = document.getElementById('contact-show');
-let hide = document.getElementById('contact-hide');
+//========== contact hiding button ==========//
+let contactShow = document.getElementById('contact-show');
+let contactHide = document.getElementById('contact-hide');
+let contactContent = document.getElementById('contact-content');
 
-let content = document.getElementById('contact-content');
+contactShow.style.display = 'none'; 
 
-show.style.display = 'none'; 
-
-function hideContent() {
-    content.style.display = 'none';
-    show.style.display = ''; 
-    hide.style.display = 'none'; 
+function hideContactContent() {
+    contactContent.style.display = 'none';
+    contactShow.style.display = ''; 
+    contactHide.style.display = 'none'; 
 }
 
-hide.addEventListener('click', hideContent);
+contactHide.addEventListener('click', hideContactContent);
 
-const showContent = () => {
-    content.style.display = '';
-    show.style.display = 'none'; 
-    hide.style.display = ''; 
+const showContactContent = () => {
+    contactContent.style.display = '';
+    contactShow.style.display = 'none'; 
+    contactHide.style.display = ''; 
+};
+contactShow.onclick = showContactContent;
+
+//========== software&languages hiding button ==========//
+
+let softwareSkillShow = document.getElementById('software-skill-show');
+let softwareSkillHide = document.getElementById('software-skill-hide');
+let softwareSkillContent = document.getElementById('software-skill-content');
+
+softwareSkillShow.style.display = 'none';
+
+const hideSoftwareSkillContent = () => {
+    softwareSkillContent.style.display ='none';
+    softwareSkillHide.style.display = 'none';
+    softwareSkillShow.style.display = '';
 };
 
-show.onclick = showContent;
+softwareSkillHide.onclick = hideSoftwareSkillContent;
 
+const showSoftwareSkillContent = () => {
+    softwareSkillContent.style.display = '';
+    softwareSkillHide.style.display = '';
+    softwareSkillShow.style.display = 'none';
+};
 
-
+softwareSkillShow.onclick = showSoftwareSkillContent;
