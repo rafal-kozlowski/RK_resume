b3fac6bee1ed6c2f8e1bdfdb382d4f052050f31f
Rafal Kozlowski <raffal.kozlowski@gmail.com>
2022-09-01 19:59:57 +0200

    add hiding buttons

--- main.js
+++ main.js
@@ -68,3 +68,98 @@ const showSoftSkillContent = () => {
 
 softSkillShow.onclick = showSoftSkillContent;
 
+//========== additional activities hiding button ==========//
+
+let activitiesShow = document.getElementById('activities-show');
+let activitiesHide = document.getElementById('activities-hide');
+let activitiesContent = document.getElementById('activities-content');
+
+activitiesShow.style.display = 'none';
+
+const hideActivitiesContent = () => {
+    activitiesContent.style.display ='none';
+    activitiesHide.style.display = 'none';
+    activitiesShow.style.display = '';
+};
+
+activitiesHide.onclick = hideActivitiesContent;
+
+const showActivitiesContent = () => {
+    activitiesContent.style.display = '';
+    activitiesHide.style.display = '';
+    activitiesShow.style.display = 'none';
+};
+
+activitiesShow.onclick = showActivitiesContent;
+
+//========== interest hiding button ==========//
+
+let interestShow = document.getElementById('interest-show');
+let interestHide = document.getElementById('interest-hide');
+let interestContent = document.getElementById('interest-content');
+
+interestShow.style.display = 'none';
+
+const hideInterestContent = () => {
+    interestContent.style.display ='none';
+    interestHide.style.display = 'none';
+    interestShow.style.display = '';
+};
+
+interestHide.onclick = hideInterestContent;
+
+const showInterestContent = () => {
+    interestContent.style.display = '';
+    interestHide.style.display = '';
+    interestShow.style.display = 'none';
+};
+
+interestShow.onclick = showInterestContent;
+
+//========== education hiding button ==========//
+
+let educationShow = document.getElementById('education-show');
+let educationHide = document.getElementById('education-hide');
+let educationContent = document.getElementById('education-content');
+
+educationShow.style.display = 'none';
+
+const hideEducationContent = () => {
+    educationContent.style.display ='none';
+    educationHide.style.display = 'none';
+    educationShow.style.display = '';
+};
+
+educationHide.onclick = hideEducationContent;
+
+const showEducationContent = () => {
+    educationContent.style.display = '';
+    educationHide.style.display = '';
+    educationShow.style.display = 'none';
+};
+
+educationShow.onclick = showEducationContent;
+
+//========== emplyment hiding button ==========//
+
+let employmentShow = document.getElementById('employment-show');
+let employmentHide = document.getElementById('employment-hide');
+let employmentContent = document.getElementById('employment-content');
+
+employmentShow.style.display = 'none';
+
+const hideEmploymentContent = () => {
+    employmentContent.style.display ='none';
+    employmentHide.style.display = 'none';
+    employmentShow.style.display = '';
+};
+
+employmentHide.onclick = hideEmploymentContent;
+
+const showEmploymentContent = () => {
+    employmentContent.style.display = '';
+    employmentHide.style.display = '';
+    employmentShow.style.display = 'none';
+};
+
+employmentShow.onclick = showEmploymentContent;
